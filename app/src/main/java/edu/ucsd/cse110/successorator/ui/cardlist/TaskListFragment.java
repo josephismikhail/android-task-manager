937eb24a56079c90fbad8c9791c300c064b87b15
JuanYin1 <79886525+JuanYin1@users.noreply.github.com>
2024-02-15 15:48:00 -0800

    1. Add code on the TaskListFragment.java file. 2. Display the date on the top of the screen as "Weekdays Month/date" * Already tested that the date will update after 2AM

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/cardlist/TaskListFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/cardlist/TaskListFragment.java
@@ -17,6 +17,14 @@ import edu.ucsd.cse110.successorator.MainViewModel;
 import edu.ucsd.cse110.successorator.databinding.FragmentTaskListBinding;
 import edu.ucsd.cse110.successorator.ui.cardlist.dialog.CreateTaskDialogFragment;
 
+// library for showing date
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import android.widget.TextView;
+import edu.ucsd.cse110.successorator.R;
+
 public class TaskListFragment extends Fragment {
     private FragmentTaskListBinding view;
     private MainViewModel activityModel;
@@ -82,6 +90,31 @@ public class TaskListFragment extends Fragment {
             var dialogFragment = CreateTaskDialogFragment.newInstance();
             dialogFragment.show(getParentFragmentManager(), "CreateTaskDialogFragment");
         });
+
+        TextView dateTextView = view.getRoot().findViewById(R.id.date);
+        Calendar calendar = Calendar.getInstance();
+        Date currentTime = calendar.getTime();
+
+        // Set the time to 2 AM
+        calendar.set(Calendar.HOUR_OF_DAY, 2);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        // Check if the current time is before 2 AM, then add one day
+        if (currentTime.before(calendar.getTime())) {
+            calendar.add(Calendar.DAY_OF_MONTH, -1);
+        }
+
+        // Get the updated date and time
+        Date updatedTime = calendar.getTime();
+
+        // Format the date for display
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE MM/dd", Locale.getDefault());
+        String updatedTimeString = sdf.format(updatedTime);
+
+        // Update the text of the TextView with the formatted date
+        dateTextView.setText(updatedTimeString);
 
         return view.getRoot();
     }
