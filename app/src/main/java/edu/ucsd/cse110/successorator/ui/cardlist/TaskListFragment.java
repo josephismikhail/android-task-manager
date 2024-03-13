c8a072698478ffe72a43e98e520159ab753fa708
Sebastian <sebastiandiaz1222@gmail.com>
2024-03-12 19:53:50 -0700

    Filter by context implemented, removed text from Focus Mode spinner

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/cardlist/TaskListFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/cardlist/TaskListFragment.java
@@ -171,6 +171,9 @@ public class TaskListFragment extends Fragment{
         focusModeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Hide spinner text
+                ((TextView)view).setText(null);
+
                 // Get the selected item
                 String selectedItem = (String) parent.getItemAtPosition(position);
 
